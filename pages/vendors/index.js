5dac5aa5cf5cd7b07c0ec079859f134ebb99b1b4
Craig bauer <craigbauer23@gmail.com>
2021-01-30 21:19:11 -0500

    changed auth redirect route

--- pages/vendors/index.js
+++ pages/vendors/index.js
@@ -99,7 +99,7 @@ function Vendors({session}) {
     }
 
     firebaseClient();
-    console.log(session);
+    //console.log(session);
     if(session) {
         return (
             <Fragment>
@@ -139,7 +139,7 @@ export async function getServerSideProps(context) {
             props: {session: `Email: ${email} IUD: ${uid}`}
         }
     } catch (err) {
-        context.res.writeHead(302, {location: "/login"}); //login //change to welcome page
+        context.res.writeHead(302, {location: "/"}); //login //change to welcome page
         context.res.end();
         return (
             { props: [] }
